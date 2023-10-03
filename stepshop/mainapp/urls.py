ece5ba71e90802cabdc3c12b4034acc5bf06e71c
Умирова Гаухар Даулеткановна <@itstep.loc>
2023-10-03 17:50:59 +0600

    lesson_02

--- stepshop/mainapp/urls.py
+++ stepshop/mainapp/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from mainapp.views import index, about, contacts, product, products
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
+    path('', index),
+    path('about/', about),
+    path('contacts/', contacts),
+    path('product/', product),
+    path('products/', products),
 ]
