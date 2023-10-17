106e99a639ff78477aa18b6c7dcfd58db1bd4f78
Умирова Гаухар Даулеткановна <@itstep.loc>
2023-10-17 17:39:58 +0600

    lesson_04

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -2,21 +2,38 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    title = "главная"
+    links_menu = [
+        {'link': 'index', 'name': 'Главная'},
+        {'link': 'about', 'name': 'О нас'},
+        {'link': 'products:index', 'name': 'Продукты'},
+        {'link': 'contacts', 'name': 'Контакты'},
+    ]
+    context = {'title': title,
+               'links_menu': links_menu, }
+    return render(request, 'index.html', context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    title = "о нас"
+    context = {'title': title}
+    return render(request, 'about.html', context)
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    title = "контакты"
+    context = {'title': title}
+    return render(request, 'contacts.html', context)
 
 
 def product(request):
-    return render(request, 'product.html')
+    title = "каталог продуктов"
+    context = {'title': title}
+    return render(request, 'product.html', context)
 
 
 def products(request):
-    return render(request, 'products.html')
+    title = "страница продукта"
+    context = {'title': title}
+    return render(request, 'products.html', context)
 
