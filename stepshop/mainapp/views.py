13fe56a66c970f6d8edd3971660f4957c937c357
Умирова Гаухар Даулеткановна <@step.kz>
2023-12-05 17:39:18 +0600

    lesson_11

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
 from mainapp.models import Product, Category
 
 
@@ -17,46 +18,58 @@ def get_data(**kawargs):
     context.update(**kawargs)
     return context
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    return []
+
 
 def index(request):
-    title = "главное"
+    title = "Главная"
+    basket = get_basket(request.user)
     prods = Product.objects.all()
-    context = get_data(title=title, prods=prods)
+    context = get_data(title=title, prods=prods, basket=basket)
     return render(request, 'index.html', context)
 
 
 def about(request):
     title = "О нас"
-    context = get_data(title=title)
+    basket = get_basket(request.user)
+    context = get_data(title=title,  basket=basket)
     return render(request, 'about.html', context)
 
 
 def contacts(request):
     title = "Связаться с нами"
-    context = get_data(title=title)
+    basket = get_basket(request.user)
+    context = get_data(title=title, basket=basket)
     return render(request, 'contacts.html', context)
 
 
 def product(request, pk):
     title = "Продукт"
+    basket = get_basket(request.user)
     prod = Product.objects.get(pk=pk)
-    context = get_data(title=title, prod=prod)
+    same_prods = Product.objects.filter(category=prod.category).exclude(pk=pk)
+    context = get_data(title=title, prod=prod, same_prods=same_prods, basket=basket)
     return render(request, 'product.html', context)
 
 
 def products(request, pk=None):
-    title = "Котолог продуктов"
+    title = "Каталог продуктов"
 
     #_products = Product.objects.all()
     _products = Product.objects.order_by('price')
     context = {}
+
+    basket = get_basket(request.user)
 
     if pk is not None:
         category = get_object_or_404(Category, pk=pk)
         _products = Product.objects.filter(category__pk=pk).order_by('price')
         context = get_data(category=category)
 
-    context = get_data(title=title, prods=_products, **context)
+    context = get_data(title=title, prods=_products, basket=basket, **context)
     return render(request, 'products.html', context)
 
 
